d7d8a8b19511fc9a4f80813ce4fb210175de0f69
AbhiDhir <dhir.abhimanyu@gmail.com>
2020-08-10 19:34:27 -0500

    Significantly Cleans up code and adds documentation

---
+++ cache_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import yfinance as yf
+import pickle
+
+## Constants ##
+startDay = "2010-01-01"
+endDay = "2020-08-01"
+
+## Get S&P 500 Tickers ##
+table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+tickers = table[0]['Symbol'].to_numpy()
+# Removes '.' characters from tickers to support yfinacne queries
+tickers = [stock.replace(".", "") for stock in tickers]
+
+## Dowload Data ##
+history = []
+# yfinance only supports donwloading history from 250 stocks at a time
+batchSize = 250
+for i in range(0,len(tickers),batchSize):
+    companies = yf.Tickers(" ".join(tickers[i:min(i+batchSize,len(tickers))]))
+    history.append(companies.history(start=startDay, end=endDay))
+full_history = pd.concat(history, axis=1)
+
+## Clean Data ##
+# drop full columns and rows of NaN
+full_history.dropna(axis=0, how='all')
+full_history.dropna(axis=1, how='all')
+
+## Cache Data ##
+pickle.dump(full_history, open("stock_history", "wb"))
--- ticker_correlation.py
+++ ticker_correlation.py
@@ -1,4 +1,3 @@
-import yfinance as yf
 import numpy as np
 import math
 import itertools
@@ -11,81 +10,114 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 np.set_printoptions(precision=4, suppress=True)
 
-# table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-# tickers = np.random.choice(table[0]['Symbol'].to_numpy(), 250, replace=False)
-# tickers = [stock.replace(".", "") for stock in tickers]
+## Constants ##
+tickers = ["MSFT", "GOOGL", "AMZN", "AAL", "BA", "TWTR"]
+# useSpy = False
+# numCompsSpy = 100
+startDay = "2010-01-01"
+endDay = "2020-08-01"
+startAmount = 1000000
 
-tickers = ["MSFT", "AAPL", "PLNT", "BA", "WM","AMZN","ENPH"]
+## Download non-cached tickers and add them to cache ##
 
-startDay = "2015-07-01"
-endDay = "2020-07-01"
-start_datetime = datetime.strptime(startDay, '%Y-%m-%d')
-end_datetime = datetime.strptime(endDay, '%Y-%m-%d')
-diff = relativedelta(end_datetime, start_datetime)
-time_diff = diff.years + (diff.months*30 + diff.days) / 365.25 
+## Get Stock Data ##
+history = pickle.load(open('stock_history', 'rb'))
+close_history = history['Close'].dropna(how='all')
+dates = close_history['AAPL'].keys()
 
-companies = yf.Tickers(" ".join(tickers))
-history = companies.history(start=startDay, end=endDay)
+## Setup index_to_ticker dict ##
+index_to_ticker = {i: ticker for i,ticker in enumerate(tickers)}
 
-returnsMatrix = [(history['Close'][ticker][-1]/history['Close'][ticker][0])**(1/time_diff)-1 for ticker in tickers]
-matrix = np.array([np.array(history['Close'][ticker]) for ticker in tickers])
-matrix = np.array([np.array([(x[i]-x[i-1])/x[i-1] for i in range(1,len(x))]) for x in matrix])
-corMatrix = np.corrcoef(matrix)
-variances = [(math.sqrt(252)*np.std(ticker))**2 for ticker in matrix]
+## Get start and end dates for each stock ##
+dateRanges = {}
+for ticker in tickers:
+    dates = close_history[ticker].dropna(how="any").keys()
+    dateRanges[ticker] = (dates[0], dates[-1])
 
+## Get CAGR for stocks ##
+returnsDict = {}
+for ticker in tickers:
+    start = dateRanges[ticker][0]
+    end = dateRanges[ticker][1]
+    diff = relativedelta(end, start)
+    years = diff.years + (diff.months*30 + diff.days) / 365.25 
+    # follows formula (end price / start price)^(1/years) - 1
+    returnsDict[ticker] = (close_history[ticker][end]/close_history[ticker][start])**(1/years)-1
+
+## Get Day-To-Day percent change for each stock over time period ##
+percentChange = { ticker: close_history[ticker][dateRanges[ticker][0]:] for ticker in tickers}
+for ticker in tickers:
+    data = percentChange[ticker]
+    # formula is (today price - yesterday price) / yesterday price
+    percentChange[ticker] = np.array([(data[i]-data[i-1])/data[i-1] for i in range(len(data))])
+
+## Get Variances of each stock ##
+variances = {ticker: (math.sqrt(252)*np.std(percentChange[ticker]))**2 for ticker in tickers}
+
+## Get Correlation Matrix for all stock data ##
+corMatrix = np.zeros((len(tickers), len(tickers)))
+for pair in list(itertools.combinations(range(len(tickers)),2)):
+    if dateRanges[tickers[pair[0]]][1] != dateRanges[tickers[pair[1]]][1]:
+        raise Exception("Tickers do not all end on the same day")
+
+    # if stocks have existed for different amount of times, compares correct timeframe
+    startIdx = min(len(percentChange[tickers[pair[0]]]),len(percentChange[tickers[pair[1]]]))
+    temp = np.array([
+        percentChange[tickers[pair[0]]][-1*startIdx:],
+        percentChange[tickers[pair[1]]][-1*startIdx:]
+    ])
+    corMatrix[pair] = np.corrcoef(temp)[0,1]
+
+## Solve for portfolio weights ##
+# Returns portfolio variance
 def minimizer(weights):
-    port = []
-    arr = []
+    varianceArray = []
+    returnsArray = []
+    safeIncomeRate = 0.02
     for i in range(len(tickers)):
-        port.append((weights[i]**2)*variances[i])
-        arr.append(returnsMatrix[i]*weights[i])
+        varianceArray.append((weights[i]**2)*variances[index_to_ticker[i]])
+        returnsArray.append(returnsDict[index_to_ticker[i]]*weights[i])
     for pair in list(itertools.combinations(range(len(tickers)), 2)):
-        port.append(2*weights[pair[0]]*weights[pair[1]]*math.sqrt(variances[pair[0]])*math.sqrt(variances[pair[1]])*corMatrix[pair[0],pair[1]])
-    return -1*((sum(arr) - .02) / sum(port))
+        varianceArray.append(2*weights[pair[0]]
+                            *weights[pair[1]]
+                            *math.sqrt(variances[index_to_ticker[pair[0]]])
+                            *math.sqrt(variances[index_to_ticker[pair[1]]])
+                            *corMatrix[pair])
+    # The value is multiplied by -1 so that it can be minimized
+    return -1*((sum(returnsArray) - safeIncomeRate) / sum(varianceArray))
 
-def con(t):
+def add_to_1_constraint(t):
     return sum(t) - 1
 
 bounds = [(0,1) for i in range(len(tickers))]
 
-cons = {'type':'eq', 'fun': con}
-x0 = np.random.dirichlet(np.ones(len(tickers)),size=1)
+cons = {'type':'eq', 'fun': add_to_1_constraint}
+# Initialize weights
+x0 = np.array([1/len(tickers) for i in range(len(tickers))])
 
-weights = np.array(scipy.optimize.minimize(minimizer, x0, bounds=bounds, constraints=cons).x)
-tickers = sorted([(weights[i], tickers[i]) for i in range(len(tickers))], key=lambda x:x[0])
-print(tickers)
+weights = scipy.optimize.minimize(minimizer, x0, bounds=bounds, constraints=cons).x
+ticker_weights = sorted([(tickers[i], weights[i]) for i in range(len(tickers))], key=lambda x:x[1], reverse=True)
+print(np.array([(i[0], np.round(i[1], decimals=4)) for i in ticker_weights]))
 
-# tickers = pickle.load(open("company_weights", 'rb'))
-# tickers = [x[1] for x in weights]
-# tickers = [(.5,"MSFT"), (.5,"GOOGL")]
-startAmount = 1000000
+## Calculate Shares for each stock at startDay ##
+# For stocks that were not listed at the start day, their initial price is used"
+stock_prices = { ticker: close_history[ticker][dateRanges[ticker][0]:] for ticker in tickers}
+shares = {ticker[0]: startAmount*ticker[1]/stock_prices[ticker[0]][0] for ticker in ticker_weights}
 
-# startDay = "2015-01-01"
-# endDay = "2020-07-01"
-# start_datetime = datetime.strptime(startDay, '%Y-%m-%d')
-# end_datetime = datetime.strptime(endDay, '%Y-%m-%d')
-# diff = relativedelta(end_datetime, start_datetime)
-# time_diff = diff.years + (diff.months*30 + diff.days) / 365.25 
-
-# companies = yf.Tickers(" ".join([ticker[1] for ticker in tickers]))
-# history = companies.history(start=startDay, end=endDay)
-
-shares = {ticker[1]: startAmount*ticker[0]/history['Close'][ticker[1]][0] for ticker in tickers}
-data = [history['Close'][ticker[1]]*shares[ticker[1]] for ticker in tickers]
+## Create portfolio weighted stock data ##
+data = []
+for ticker in ticker_weights:
+    startingSharePrice = stock_prices[ticker[0]][0]
+    data.append(np.nan_to_num(np.array(close_history[ticker[0]]*shares[ticker[0]]),startingSharePrice))
 data = np.array(data)
-mask = np.all(np.isnan(data), axis=1)
-data = data[~mask]
-mask = np.all(np.isnan(data), axis=0)
-data = data[:,~mask]
-# data2 = np.nan_to_num(data2, nan=0)
 accData = [sum(data[:,i]) for i in range(len(data[0]))]
 y = np.array(accData)
-print(y[0], y[-1])
 x = np.array([i for i in range(len(accData))])
+print(f'\nStarting Amount: ${np.round(y[0], decimals=2)}', f'Ending Amount: ${np.round(y[-1], decimals=2)}')
 
+## Plot Graph ##
 plt.plot(x,y)
-plt.xlabel("Date")
-plt.ylabel("Adjusted")
+plt.xlabel("Time")
+plt.ylabel("Dollars")
 plt.title("Price data")
-plt.style.use('dark_background')
 plt.show()
